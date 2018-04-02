bdf2a5eca64f77dc39aada2305809f66b60174a0
ccorrado <ccorrad@its.jnj.com>
2018-04-01 21:48:43 -0400

    Updating sample.

--- app/src/main/java/com/ccorrads/prestonsample/SampleActivity.kt
+++ app/src/main/java/com/ccorrads/prestonsample/SampleActivity.kt
@@ -2,108 +2,40 @@ package com.ccorrads.prestonsample
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import com.ccorrads.preston.media.MediaPlaybackService
-import com.ccorrads.preston.media.MediaPlayerObserver
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.schedulers.Schedulers
+import android.support.v7.widget.LinearLayoutManager
+import com.ccorrads.prestonsample.mediacards.MediaItem
+import com.ccorrads.prestonsample.mediacards.SampleRVAdapter
 import kotlinx.android.synthetic.main.activity_sample.*
 
 class SampleActivity : AppCompatActivity() {
 
-    private val mediaService: MediaPlaybackService = MediaPlaybackService()
-    private val mediaPlayerObserver: MediaPlayerObserver = MediaPlayerObserver()
-
-    private val audioPath = "sample_audio/might_be_coffee.mp3"
-
-    private var muted = false
+    private var adapter: SampleRVAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sample)
 
-        action_stop.setOnClickListener { stopMedia() }
+        val sourcePaths = resources.getStringArray(R.array.source_paths)
+        val sourceTitles = resources.getStringArray(R.array.source_titles)
+        val mediaItems = ArrayList<MediaItem>()
 
-        action_replay.setOnClickListener { playMedia(muted) }
-
-        play_pause_toggle.setOnClickListener { pauseOrResumeMedia(muted) }
-
-        mute_toggle.setOnCheckedChangeListener { _, checked ->
-            muted = checked
-            if (muted) {
-                muteCurrentMP()
-            } else {
-                unmuteCurrentMP()
-            }
+        for ((index, path) in sourcePaths.withIndex()) {
+            val item = MediaItem(path, sourceTitles[index])
+            mediaItems.add(item)
         }
+
+        sample_rv.layoutManager = LinearLayoutManager(this)
+        adapter = SampleRVAdapter(this, mediaItems)
+        sample_rv.adapter = adapter
     }
 
     override fun onPause() {
         super.onPause()
-        pauseOrResumeMedia(false)
+        adapter?.onPauseResume()
     }
 
     override fun onResume() {
         super.onResume()
-        pauseOrResumeMedia(false)
-    }
-
-    /**
-     * Play a Media Player with some assets path.
-     */
-    private fun playMedia(muted: Boolean) {
-        mediaService.create(applicationContext, audioPath,
-                MediaPlaybackService.StateAction.ACTION_PLAY, muted)
-                .mediaPlayerObservable
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(mediaPlayerObserver)
-    }
-
-    /**
-     * Pause or Resume a running Media Player
-     */
-    private fun pauseOrResumeMedia(muted: Boolean) {
-        mediaService.create(applicationContext, audioPath,
-                MediaPlaybackService.StateAction.ACTION_PAUSE_OR_RESUME, muted)
-                .mediaPlayerObservable
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(mediaPlayerObserver)
-    }
-
-    /**
-     * Stop a Media Player.
-     */
-    private fun stopMedia() {
-        mediaService.create(applicationContext, audioPath,
-                MediaPlaybackService.StateAction.ACTION_STOP, false)
-                .mediaPlayerObservable
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(mediaPlayerObserver)
-    }
-
-    /**
-     * Muting a running Media Player without playing or pausing.
-     */
-    private fun muteCurrentMP() {
-        mediaService.create(applicationContext, audioPath,
-                MediaPlaybackService.StateAction.ACTION_MUTE, true)
-                .mediaPlayerObservable
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(mediaPlayerObserver)
-    }
-
-    /**
-     * Unmuting a running Media Player without playing or pausing.
-     */
-    private fun unmuteCurrentMP() {
-        mediaService.create(applicationContext, audioPath,
-                MediaPlaybackService.StateAction.ACTION_UNMUTE, false)
-                .mediaPlayerObservable
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(mediaPlayerObserver)
+        adapter?.onPauseResume()
     }
 }
---
+++ app/src/main/java/com/ccorrads/prestonsample/mediacards/SampleRVAdapter.kt
@@ -0,0 +1,115 @@
+package com.ccorrads.prestonsample.mediacards
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.ccorrads.preston.media.MediaPlaybackService
+import com.ccorrads.preston.media.MediaPlayerObserver
+import com.ccorrads.prestonsample.R
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.card_media_item.view.*
+
+class SampleRVAdapter(private var context: Context, private var mediaItems: List<MediaItem>) : RecyclerView.Adapter<MediaViewHolder>() {
+
+    private val mediaService: MediaPlaybackService = MediaPlaybackService()
+    private val mediaPlayerObserver: MediaPlayerObserver = MediaPlayerObserver()
+
+    private var muted: Boolean = false
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): MediaViewHolder {
+        val itemView = LayoutInflater.from(viewGroup.context).inflate(R.layout.card_media_item,
+                viewGroup, false)
+        return MediaViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: MediaViewHolder, position: Int) {
+        val mediaItem: MediaItem = mediaItems[position]
+        holder.itemView.song_title.text = mediaItem.title
+
+        holder.itemView.action_stop.setOnClickListener { stopMedia(mediaItem.sourcePath) }
+
+        holder.itemView.action_replay.setOnClickListener { playMedia(muted, mediaItem.sourcePath) }
+
+        holder.itemView.play_pause_toggle.setOnClickListener { pauseOrResumeMedia(muted, mediaItem.sourcePath) }
+
+        holder.itemView.mute_toggle.setOnCheckedChangeListener { _, checked ->
+            muted = checked
+            if (muted) {
+                muteCurrentMP(mediaItem.sourcePath)
+            } else {
+                unmuteCurrentMP(mediaItem.sourcePath)
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        val meals: List<MediaItem> = mediaItems
+        return meals.size
+    }
+
+    fun onPauseResume() {
+        pauseOrResumeMedia(false, "")
+    }
+
+    /**
+     * Play a Media Player with some assets path.
+     */
+    private fun playMedia(muted: Boolean, audioPath: String) {
+        mediaService.create(context, audioPath,
+                MediaPlaybackService.StateAction.ACTION_PLAY, muted)
+                .mediaPlayerObservable
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(mediaPlayerObserver)
+    }
+
+    /**
+     * Pause or Resume a running Media Player
+     */
+    private fun pauseOrResumeMedia(muted: Boolean, audioPath: String) {
+        mediaService.create(context, audioPath,
+                MediaPlaybackService.StateAction.ACTION_PAUSE_OR_RESUME, muted)
+                .mediaPlayerObservable
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(mediaPlayerObserver)
+    }
+
+    /**
+     * Stop a Media Player.
+     */
+    private fun stopMedia(audioPath: String) {
+        mediaService.create(context, audioPath,
+                MediaPlaybackService.StateAction.ACTION_STOP, false)
+                .mediaPlayerObservable
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(mediaPlayerObserver)
+    }
+
+    /**
+     * Muting a running Media Player without playing or pausing.
+     */
+    private fun muteCurrentMP(audioPath: String) {
+        mediaService.create(context, audioPath,
+                MediaPlaybackService.StateAction.ACTION_MUTE, true)
+                .mediaPlayerObservable
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(mediaPlayerObserver)
+    }
+
+    /**
+     * Unmuting a running Media Player without playing or pausing.
+     */
+    private fun unmuteCurrentMP(audioPath: String) {
+        mediaService.create(context, audioPath,
+                MediaPlaybackService.StateAction.ACTION_UNMUTE, false)
+                .mediaPlayerObservable
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(mediaPlayerObserver)
+    }
+}
